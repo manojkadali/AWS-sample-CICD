18df4d8bd323171358241ac1760e3dc5b1c79134
manojkadali <160770025+manojkadali@users.noreply.github.com>
2024-07-15 01:36:30 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,14 +3,4 @@
 set -e
 
 # Get the IDs of running containers (excluding the header)
-container_ids=$(docker ps -q)
-
-if [ -z "$container_ids" ]; then
-    echo "No containers are running."
-else
-    # Stop and remove each running container
-    for id in $container_ids; do
-        docker rm -f "$id"
-    done
-    echo "All running containers have been stopped and removed."
-fi
+echo "hii"
